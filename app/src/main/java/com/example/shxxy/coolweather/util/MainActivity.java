33339aaa739eec45afbe4feb9513a4ac43a737f7
Tony <tony@gmail.com>
2018-05-08 23:14:06 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/shxxy/coolweather/util/MainActivity.java
+++ app/src/main/java/com/example/shxxy/coolweather/util/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.shxxy.coolweather;
+package com.example.shxxy.coolweather.util;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.shxxy.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
