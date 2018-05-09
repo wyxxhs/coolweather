59ca35f83d3f174138f8c3aab587c58e8f040da5
Tony <tony@gmail.com>
2018-05-09 10:39:06 +0800

    Revert "加入创建数据库和表的各项配置"
    This reverts commit 33339aaa739eec45afbe4feb9513a4ac43a737f7.

--- app/src/main/java/com/example/shxxy/coolweather/MainActivity.java
+++ app/src/main/java/com/example/shxxy/coolweather/MainActivity.java
@@ -1,9 +1,7 @@
-package com.example.shxxy.coolweather.util;
+package com.example.shxxy.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.shxxy.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
